610ac145bb8cdf06d95997216a4c220ec40b8cb2
zhangke43 <zhangke43@meituan.com>
2022-09-29 14:30:53 +0800

    scala中函数的基本构建

--- function/Definition.scala
+++ function/Definition.scala
@@ -31,5 +31,32 @@ object Definition {
       return f"姓名为:${name}, 年龄为:${age}"
     }
     println(f4("zhangke", 25))
+
+    println("================================")
+    // 5.可变参数的使用（传入若干个值，一般放在最后）
+    def f5(age: Int, name: String*): Unit = {
+      println("这是一个包含可变参数的函数")
+      println(s"您输入的年龄为:${age}, 您输入的字符为:${name}")
+    }
+    print(f5(25,"zhangke", "lizhenhua"))
+
+    println("================================")
+    // 6.带有默认值的参数（放在必填项后面）
+    def f6(age:Int, name:String = "zhangke"): Unit = {
+      println("这是一个带默认值参数的函数")
+      println(s"姓名${name}, 年龄${age}")
+    }
+    println(f6(25))
+    println(f6(18, name="lizhenhua"))
+
+    println("================================")
+    // 7.带上参数名称，可以在任意位置放置
+
+    def f7(age:Int, name:String): Unit = {
+      println("这是一个函数参数名称的测试")
+      println(s"姓名-${name}, 年龄-${age}")
+    }
+    print(f7(name="zhangke", age=25))
+
   }
 }
